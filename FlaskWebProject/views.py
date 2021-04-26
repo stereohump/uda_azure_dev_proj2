4fbdf9f04791b8a1febe60d8a36448a6a5bde466
stereohump <wachhold@yahoo.com>
2021-04-25 19:49:36 -0700

    fixed logging entry in views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -75,7 +75,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
-        app.logger.info("User succefully logged in.")
+        app.logger.info("User logged in successfully.")
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
